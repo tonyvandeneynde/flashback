278538e91b750618bbb57c24a61d3968bcf32055
Tony Van den Eynde <tonyvandeneynde@gmail.com>
2025-05-26 15:19:19 +0200

    refactor upload service to Nestjs

--- flashback-api/src/folders/folder.module.ts
+++ flashback-api/src/folders/folder.module.ts
@@ -2,26 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { AuthModule } from 'src/auth';
 import { FolderService } from './folder.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {
-  Account,
-  Folder,
-  Gallery,
-  Image,
-  Tag,
-  User,
-} from 'src/database/entities';
 import { GalleryModule } from 'src/gallery';
 import { ImageService } from 'src/images/image.service';
 import { RabbitMQService } from 'src/rabbitMQ';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    AuthModule,
-    GalleryModule,
-    TypeOrmModule.forFeature([Folder, Account, Gallery, User, Image, Tag]),
-  ],
+  imports: [DatabaseModule, AuthModule, GalleryModule],
   providers: [FolderService, ImageService, RabbitMQService],
   exports: [FolderService],
 })
